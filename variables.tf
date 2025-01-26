6da88cbf8081d4ef1e4b79089d6819259360f0a5
Felipe Ribeiro <felipeespindola.ti@gmail.com>
2025-01-26 07:38:13 -0300

    fix: adjusting typo

--- variables.tf
+++ variables.tf
@@ -220,11 +220,11 @@ variable "scale_tracking_requests" {
 variable "efs_volumes" {
   type = list(object({
     volume_name : string
-    file_system_id: string
-    fyle_system_root: string
-    mount_point: string
+    file_system_id : string
+    file_system_root : string
+    mount_point : string
     read_only : bool
   }))
-  default = []
+  description = "Volumes EFS existentes para serem montados nas tasks do ECS"
+  default     = []
 }
-
